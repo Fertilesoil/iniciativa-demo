b49c0287d8607d7596a2bd23f71251e8ea3d5c53
Fertilesoil <gasnbloop@gmail.com>
2023-10-27 15:06:55 -0300

    Card 1

---
+++ projeto-demo/src/App.tsx
@@ -0,0 +1,14 @@
+import Header from './pages/header/Header';
+import Body from './pages/body/Body'
+
+function App() {
+
+  return (
+    <>
+      <Header />
+      <Body />
+    </>
+  )
+}
+
+export default App
---
+++ projeto-demo/src/pages/header/Header.tsx
@@ -0,0 +1,22 @@
+﻿import { } from './Header.css'
+
+function Header() {
+   return (
+      <header>
+         <nav id="nav-flex">
+            <a className="link-nome" href="#">Iniciativa Atena</a>
+            <div id='div-flex'>
+               <ul id="ul-links">
+                  <li><a className="link-format" href="#">Produtos</a></li>
+                  <li><a className="link-format" href="#">Noticias</a></li>
+                  <li><a className="link-format" href="#">Quem Somos</a></li>
+                  <li><a className="link-format" href="#">Meu Perfil</a></li>
+                  <li><a className="link-format" href="#">Carrinho</a></li>
+               </ul>
+            </div>
+         </nav>
+      </header>
+   )
+}
+
+export default Header
